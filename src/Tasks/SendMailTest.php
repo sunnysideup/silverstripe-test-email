e0f980464cbdfef2ee0e9978b35427a9d8b68bf0
Tristan <ghoststeam217@gmail.com>
2020-05-22 14:42:07 +1200

    Create SendMailTest.php

---
+++ src/Tasks/SendMailTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace StainMaster\App\Tasks;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Control\Director;
+use SilverStripe\Control\Email\Email;
+
+class SendEmailTest extends BuildTask
+{
+    private static $segment = 'sendmail';
+
+    protected $title = 'Test if emails are working';
+
+    public function run($request)
+    {
+        $from = $_GET['from'] ?? 'webmaster@'. Director::baseURL();
+        $to = $_GET['to'] ?? 'support@sunnysideup.co.nz';
+        $subject = $_GET['subject'] ?? 'testing email';
+        $message = $_GET['message'] ?? 'Message goes here';
+        echo 'from: '.$from .$this->newLine();
+        echo 'to: '.$to .$this->newLine();
+        echo 'subject: '.$subject .$this->newLine();
+        echo 'message: '.$message .$this->newLine();
+
+        $outcome = mail($to, $subject, $message);
+        echo 'PHP mail sent: ' . ($outcome ? 'YES' : 'NO') .$this->newLine();
+        $email = new Email($from, $to, $subject, $message);
+        $outcome = $email->sendPlain();
+        echo 'SS e-mail sent: ' . ($outcome ? 'YES' : 'NO') .$this->newLine();
+
+    }
+
+    protected function newLine()
+    {
+        if(Director::is_cli()) {
+            return '
+
+            ';
+        } else {
+            return '<br /><br />';
+        }
+    }
+}
