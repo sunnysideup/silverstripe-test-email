00b8ee68fb65fcfb1150a13d3d1df8d22fad96fd
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-07-13 23:10:11 +1200

    PATCH: linter

--- src/Tasks/SendMailTest.php
+++ src/Tasks/SendMailTest.php
@@ -2,43 +2,41 @@
 
 namespace Sunnysideup\EmailTest\Tasks;
 
-use SilverStripe\Dev\BuildTask;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\Email\Email;
+use SilverStripe\Dev\BuildTask;
 
-class SendEmailTest extends BuildTask
+class SendMailTest extends BuildTask
 {
-    private static $segment = 'testemail';
-
     protected $title = 'Test if emails are working';
+
+    private static $segment = 'testemail';
 
     public function run($request)
     {
-        $from = $_GET['from'] ?? 'webmaster@'. Director::baseURL();
+        $from = $_GET['from'] ?? 'webmaster@' . Director::baseURL();
         $to = $_GET['to'] ?? 'support@sunnysideup.co.nz';
         $subject = $_GET['subject'] ?? 'testing email';
         $message = $_GET['message'] ?? 'Message goes here';
-        echo 'from: '.$from .$this->newLine();
-        echo 'to: '.$to .$this->newLine();
-        echo 'subject: '.$subject .$this->newLine();
-        echo 'message: '.$message .$this->newLine();
+        echo 'from: ' . $from . $this->newLine();
+        echo 'to: ' . $to . $this->newLine();
+        echo 'subject: ' . $subject . $this->newLine();
+        echo 'message: ' . $message . $this->newLine();
 
         $outcome = mail($to, $subject, $message);
-        echo 'PHP mail sent: ' . ($outcome ? 'YES' : 'NO') .$this->newLine();
+        echo 'PHP mail sent: ' . ($outcome ? 'YES' : 'NO') . $this->newLine();
         $email = new Email($from, $to, $subject, $message);
         $outcome = $email->sendPlain();
-        echo 'SS e-mail sent: ' . ($outcome ? 'YES' : 'NO') .$this->newLine();
-
+        echo 'SS e-mail sent: ' . ($outcome ? 'YES' : 'NO') . $this->newLine();
     }
 
     protected function newLine()
     {
-        if(Director::is_cli()) {
+        if (Director::is_cli()) {
             return '
 
             ';
-        } else {
-            return '<br /><br />';
         }
+        return '<br /><br />';
     }
 }
