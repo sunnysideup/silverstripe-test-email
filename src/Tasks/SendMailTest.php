e30dfd4dc25e23ef738a0018deca63841b4f8029
Nicolaas / Sunn Side Up <github@sunnysideup.co.nz>
2021-11-11 13:08:52 +1300

    Update SendMailTest.php

--- src/Tasks/SendMailTest.php
+++ src/Tasks/SendMailTest.php
@@ -19,19 +19,33 @@ class SendMailTest extends BuildTask
         $to = $request->getVar('to') ?: 'support@' . Director::host();
         $subject = $request->getVar('subject') ?: 'testing email';
         $message = $request->getVar('message') ?: 'Message goes here';
+        if (Director::is_cli()) {
+            echo '
 
-        echo '
-            <style>
-                input {width: 80vw; max-width: 500px; padding: 5px;}
-            </style>
-            <form action="" method="get">
-                from: <br/><input name="from" value="' . Convert::raw2att($from) . '" /><br/><br/>
-                to: <br/><input name="to" value="' . Convert::raw2att($to) . '" /><br/><br/>
-                subject: <br/><input name="subject" value="' . Convert::raw2att($subject) . '" /><br/><br/>
-                message: <br/><input name="message" value="' . Convert::raw2att($message) . '" /><br/><br/>
-                <input type="submit" />
-            </form>
-        ';
+                    from: ' . Convert::raw2att($from) . '
+                    
+                    to: ' . Convert::raw2att($to) . '
+                    
+                    subject:' . Convert::raw2att($subject) . '" /><br/><br/>
+                    
+                    message: ' . Convert::raw2att($message) . '
+                    
+                    Change values like this: sake dev/tasks/testemail to=a@b.com from=c@d.com subject=test message=hello
+            ';            
+        } else {
+            echo '
+                <style>
+                    input {width: 80vw; max-width: 500px; padding: 5px;}
+                </style>
+                <form action="" method="get">
+                    from: <br/><input name="from" value="' . Convert::raw2att($from) . '" /><br/><br/>
+                    to: <br/><input name="to" value="' . Convert::raw2att($to) . '" /><br/><br/>
+                    subject: <br/><input name="subject" value="' . Convert::raw2att($subject) . '" /><br/><br/>
+                    message: <br/><input name="message" value="' . Convert::raw2att($message) . '" /><br/><br/>
+                    <input type="submit" />
+                </form>
+            ';
+        }
         if($request->getVar('from')) {
             echo '<h1>Outcome</h1>';
             $outcome = mail($to, $subject, $message);
